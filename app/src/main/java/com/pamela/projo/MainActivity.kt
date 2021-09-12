23c6d7ed6e5634af91a5ee7e01a3e1235a901c27
Selim <selimchemutai19@gmail.com>
2021-09-12 14:51:53 +0300

    Main Activity

--- app/src/main/java/com/pamela/projo/MainActivity.kt
+++ app/src/main/java/com/pamela/projo/MainActivity.kt
@@ -4,8 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFrag =
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment_content_main
+            ) as NavHostFragment
+        val navController = navHostFrag.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.NoteListFragment, R.id.NoteFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
